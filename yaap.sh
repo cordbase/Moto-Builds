14062510c2ba5ef8a559df3761683eef8f5cb213
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-21 23:20:19 +0530

    Change repo branch to 'fourteen' and clean up script
    Updated the branch from 'sixteen' to 'fourteen' and removed telephony-ext cleanup code.

--- yaap.sh
+++ yaap.sh
@@ -13,7 +13,7 @@ mkdir -p .repo/local_manifests
 curl -L -o .repo/local_manifests/yaap.xml https://raw.githubusercontent.com/cordbase/local_manifest/moto/yaap.xml
 
 # Init Rom Manifest
-repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
+repo init -u https://github.com/yaap/manifest.git -b fourteen --git-lfs
 
 # Sync the repositories  
 /opt/crave/resync.sh 
@@ -26,28 +26,6 @@ cd ../../..
 # set username
 git config --global user.name "cordbase"
 git config --global user.email "cordbase@users.noreply.github.com"
-
-# remove telephony-ext from yaap vendor
-TARGET_FILE="vendor/yaap/config/packages.mk"
-if [ -f "$TARGET_FILE" ]; then
-    sed -i '/^[[:space:]]*telephony-ext[[:space:]]*\\$/d' "$TARGET_FILE"
-    sed -i '/^[[:space:]]*telephony-ext[[:space:]]*$/d' "$TARGET_FILE"
-    echo "Removed telephony-ext entries from PRODUCT_PACKAGES and PRODUCT_BOOT_JARS."
-else
-    echo "$TARGET_FILE not found."
-    exit 1
-fi
-
-# error
-TARGET_BP="hardware/qcom-caf/sm8250/display/libdebug/Android.bp"
-EXCLUDE_PATH='$(LOCAL_PATH)/../../../../out/soong/.intermediates/vendor/yaap/build/soong/generated_kernel_includes/gen/usr/include'
-
-if [ -f "$TARGET_BP" ]; then
-    sed -i "/srcs: \[\"debug_handler.cpp\"\],/a \    exclude_include_dirs: [\"$EXCLUDE_PATH\"]," "$TARGET_BP"
-    echo "Patched $TARGET_BP to exclude generated kernel includes."
-else
-    echo "$TARGET_BP not found."
-fi
 
 # Set up build environment
 source build/envsetup.sh
